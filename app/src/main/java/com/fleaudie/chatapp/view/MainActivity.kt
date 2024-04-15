83bb8dcd5230eeec791bae283d5b1cfce5073667
fleaudie <hiedixx@gmail.com>
2024-04-15 14:21:59 +0300

    update

--- app/src/main/java/com/fleaudie/chatapp/view/MainActivity.kt
+++ app/src/main/java/com/fleaudie/chatapp/view/MainActivity.kt
@@ -2,11 +2,37 @@ package com.fleaudie.chatapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.fleaudie.chatapp.R
+import com.fleaudie.chatapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        binding.bottomNavigationView.visibility = View.GONE
+
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.chatFragment, R.id.contactsFragment, R.id.userProfileFragment -> {
+                    binding.bottomNavigationView.visibility = View.VISIBLE
+                }
+                else -> {
+                    binding.bottomNavigationView.visibility = View.GONE
+                }
+            }
+        }
+        NavigationUI.setupWithNavController(binding.bottomNavigationView,  navHostFragment.navController)
     }
 }
